f90fe900c714bd56317ff6cab3c84941ba4d5d65
6alexcn <11822127@qq.com>
2022-11-01 23:18:26 +0800

    bb

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,24 +1,24 @@
 <template>
-	<Header />
-	<Sidebar />
-	<div class="content-box" :class="{ 'content-collapse': sidebar.collapse }">
-		<Tags></Tags>
-		<div class="content">
-			<router-view v-slot="{ Component }">
-				<transition name="move" mode="out-in">
-					<keep-alive :include="tags.nameList">
-						<component :is="Component"></component>
-					</keep-alive>
-				</transition>
-			</router-view>
-		</div>
-	</div>
-</template> 
+  <Header />
+  <Sidebar />
+  <div class="content-box" :class="{ 'content-collapse': sidebar.collapse }">
+    <Tags></Tags>
+    <div class="content">
+      <router-view v-slot="{ Component }">
+        <transition name="move" mode="out-in">
+          <keep-alive :include="tags.nameList">
+            <component :is="Component"></component>
+          </keep-alive>
+        </transition>
+      </router-view>
+    </div>
+  </div>
+</template>
 
 <script setup lang="ts">
-import { useSidebarStore } from '../store/sidebar';
-import { useTagsStore } from '../store/tags';
+import { useSidebarStore } from '../store/sidebar'
+import { useTagsStore } from '../store/tags'
 
-const sidebar = useSidebarStore();
-const tags = useTagsStore();
+const sidebar = useSidebarStore()
+const tags = useTagsStore()
 </script>
